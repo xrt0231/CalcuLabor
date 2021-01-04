81d9007e519ff49c9a77740bee659dd59040c9b6
xrt0231 <jtsw@protonmail.com>
2021-01-04 10:55:09 +0800

    adjust time conversion

--- server/controllers/schedule_controller.js
+++ server/controllers/schedule_controller.js
@@ -36,10 +36,6 @@ const getActualGanttChart = async (req, res) => {
 
 const productionOrderInfo = async (req, res) => {
 	let productionOrderNum = req.body.productionOrderNum;
-	// let recordProcess = req.body.recordProcess;
-	// let outputQty = req.body.outputQty;
-	// console.log(productionOrderNum.slice(2,12))
-	console.log("controller:", productionOrderNum);
 	const schedule = (await Schedule.productionOrderInfo(productionOrderNum));
 	res.send(schedule);
 };
@@ -48,23 +44,18 @@ const createProductionOrder = async (req, res) => {
 
 	let productionOrderNum = req.body.productionOrderNum;
 	let partNum = req.body.partNum;
-
-	// let productionGroup = req.body.productionGroup;
 	let recordProcess = req.body.recordProcess;
 	let outputQty = req.body.outputQty;
 
 	//Get star time from UI and convert to DATETIME format
 	let startGet = new Date(`${req.body.start}`);
-	startGet.setHours(startGet.getHours()+8);
+	
 	let start = moment(startGet).format("YYYY-MM-DD HH:mm:ss");
     
 	//Get end time from UI and convert to DATETIME format 
 	let endGet = new Date(`${req.body.end}`);
-	endGet.setHours(endGet.getHours()+8);
+	
 	let end = moment(endGet).format("YYYY-MM-DD HH:mm:ss");
-    
-
-	console.log(productionOrderNum, partNum, recordProcess, start, end, outputQty);
 
 	const schedule = (await Schedule.createProductionOrder(productionOrderNum, partNum, recordProcess, start, end, outputQty));
 	res.send(schedule);
@@ -74,8 +65,6 @@ const updateProductionOrder = async (req, res) => {
 
 	let productionOrderNum = req.body.productionOrderNum;
 	let partNum = req.body.partNum;
-
-	// let productionGroup = req.body.productionGroup;
 	let recordProcess = req.body.recordProcess;
 	let outputQty = req.body.outputQty;
 
@@ -86,17 +75,12 @@ const updateProductionOrder = async (req, res) => {
 	//Get end time from UI and convert to DATETIME format 
 	let endGet = new Date(`${req.body.end}`);
 	let end = moment(endGet).format("YYYY-MM-DD HH:mm:ss");
-    
-
-	console.log(productionOrderNum, partNum, recordProcess, start, end, outputQty);
-
 	const schedule = (await Schedule.updateProductionOrder(productionOrderNum, partNum, recordProcess, start, end, outputQty));
 	res.send(schedule);
 }
 
 //Auto guess dropdown list of process
 const dropdownProcess = async (req, res) => {
-
 	const schedule = (await Schedule.dropdownProcess());
 	res.send(schedule);
 }
