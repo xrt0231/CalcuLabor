6c7aa3b012081d6c5e5bcf5b95efa1714b8c12e1
xrt0231 <jtsw@protonmail.com>
2021-05-21 20:23:35 +0800

    adjust apple sign in

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -92,8 +92,8 @@ const appleSignIn = async (req, res) => {
 		try {
 			const { sub: userAppleId } = await appleSignin.verifyIdToken(tokenResponse.id_token, {
 			  // Optional Options for further verification - Full list can be found here https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
-			  audience: 'lol.online.calculabor', // client id - can also be an array
-			  nonce: 'NONCE', // nonce // Check this note if coming from React Native AS RN automatically SHA256-hashes the nonce https://github.com/invertase/react-native-apple-authentication#nonce
+			  //audience: 'lol.online.calculabor', // client id - can also be an array
+			  //nonce: 'NONCE', // nonce // Check this note if coming from React Native AS RN automatically SHA256-hashes the nonce https://github.com/invertase/react-native-apple-authentication#nonce
 			  // If you want to handle expiration on your own, or if you want the expired tokens decoded
 			  ignoreExpiration: true, // default is false
 			});
