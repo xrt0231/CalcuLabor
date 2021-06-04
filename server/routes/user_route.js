078719ef4cb69c9e957fd3e4ec1f7b74ef6150b5
xrt0231 <jtsw@protonmail.com>
2021-06-04 16:22:50 +0800

    adjust signin with apple

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -15,11 +15,11 @@ router.route('/admin/signUp')
 router.route('/admin/signIn')
 	.post(signIn);
 
-router.route('/apple/redirect')
-	.post(appleSignIn)
+// router.route('/apple/redirect')
+// 	.post(appleSignIn)
 
-router.route('/apple/redirect')
-	.get(appleSignIn)
+// router.route('/apple/redirect')
+// 	.get(appleSignIn)
 
 router.route('/apple/verify')
 	.post(appleVerify)
